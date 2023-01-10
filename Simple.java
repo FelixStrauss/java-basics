d98e3373b5b362b43320dbd33e2e11aae61e36c7
FelixStrauss <117447393+FelixStrauss@users.noreply.github.com>
2023-01-10 13:52:46 +0100

    Create Simple.java

---
+++ Simple.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Simple {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int numberInput = scanner.nextInt();
+
+if (numberInput >= 5)
+        if (numberInput <= 10) {
+            System.out.println("The number" + numberInput +
+                               "lies between 5 and 10.");
+
+     }  else {
+
+            System.out.println("The number" + numberInput +
+                               "does NOT lie between 5 and 10 and higher 10.");
+     }  else {
+System.out.println("The number" + numberInput +
+                   "does NOT lie between 5 and 10 and lower 5.");
+
+        }
+
+    }
+    
+    
+}
