16176f134f07ac3937de37365ac919c93fc0c12c
FelixStrauss <117447393+FelixStrauss@users.noreply.github.com>
2023-01-11 16:54:25 +0100

    Create Scopes.java

---
+++ Scopes.java
@@ -0,0 +1,16 @@
+public class Scopes {
+    
+    public static void main (String[] args) {
+
+        int x = 5;
+
+        if (x >= 0)
+            if(x == 0)
+                System.out.println("x ist gleich 0");
+            else
+                System.out.println("x ist größer als 0");
+        System.out.println("x ist kleiner als 0");
+
+
+    }
+}
